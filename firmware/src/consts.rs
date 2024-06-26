4c60114d094f8e7a11c421faefd4526e40653d5d
eupn <eupn@protonmail.com>
2024-06-27 02:24:01 +0400

    consts: remove unused const

--- firmware/src/consts.rs
+++ firmware/src/consts.rs
@@ -9,7 +9,7 @@ pub const SHORT_NAME: &str = "Prime";
 pub const SERVICES_LIST: [[u8; 16]; 1] = [NUS_UUID.to_le_bytes()];
 
 // Buffer max bt packets
-pub const BT_BUFF_MAX_PKT_LEN: usize = 256;
 pub const BT_MAX_NUM_PKT: usize = 4;
+
 // NB! MAX_IRQ depends on chip used, for example: nRF52840 has 48 IRQs, nRF52832 has 38.
 pub const MAX_IRQ: u16 = 25;
