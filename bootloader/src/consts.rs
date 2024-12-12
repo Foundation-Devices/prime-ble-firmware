5a0f00838134354a64cfcd0e8d12901bddc373b3
Georges Palauqui <georges@foundationdevices.com>
2024-12-12 14:39:35 +0100

    bootloader: rename `boot-signed-fw` into `signed-firmware` and replace `boot-signed-fw` by `not(signed-firmware)`

--- bootloader/src/consts.rs
+++ bootloader/src/consts.rs
@@ -7,20 +7,20 @@
 pub static BOOTLOADER_ADDR: u32 = 0x27000;
 
 /// 256B are needed for cosign2 signature
-#[cfg(feature = "boot-signed-fw")]
+#[cfg(feature = "signed-firmware")]
 const SIGNATURE_HEADER_SIZE: u32 = 0x100;
 
 /// Base address of the interrupt vector table for signed firmware
 /// When booting signed firmware, the interrupt vector table is placed after SIGNATURE_HEADER_SIZE,
 /// which is after the SoftDevice but before the application code. This ensures proper
 /// interrupt handling while maintaining the security of the signed firmware.
-#[cfg(feature = "boot-signed-fw")]
+#[cfg(feature = "signed-firmware")]
 pub const INT_VECTOR_TABLE_BASE: u32 = BASE_APP_ADDR + SIGNATURE_HEADER_SIZE;
 
 /// Base address of the interrupt vector table for unsigned firmware
 /// Points to the SoftDevice base address at 0x1000, which is where the interrupt vector table
 /// must be located for unsigned firmware to properly handle interrupts through the SoftDevice
-#[cfg(feature = "boot-unsigned-fw")]
+#[cfg(not(feature = "signed-firmware"))]
 pub const INT_VECTOR_TABLE_BASE: u32 = 0x1000;
 
 /// Base address for the application in flash memory
